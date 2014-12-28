ef875e6fdaeb06b22f16ac84f41a16e3aecb88c7
sohel rana <sohel4r@gmail.com>
2014-12-29 01:22:05 +0600

    Update theme

--- child-twentyfifteen/functions.php
+++ child-twentyfifteen/functions.php
@@ -1,17 +1,28 @@
 <?php
-
+add_action( 'wp_enqueue_scripts', 'child_scripts' );
 function child_scripts() {
 	//wp_enqueue_style( $script_name, get_stylesheet_uri() );
-	$scripts = array(
-		array(
-			'name' => 'bootstrap',
-			'path' => 'bootstrap.min.js'
-		)
-	);
-		foreach ($scripts as $script ) {
-			wp_enqueue_script( $script['name'] , get_stylesheet_directory_uri() .'/js/'.$script['path']);					
-		}
+	wp_enqueue_style( "bootstrap", get_stylesheet_directory_uri().'/css/bootstrap.min.css' );	
+	wp_enqueue_script( 'bootstrap' , get_stylesheet_directory_uri() .'/js/bootstrap.min.js', array(),'', ture );					
 }
 
-wp_enqueue_style( $script_name, get_template_directory_uri().'/css/bootstrap.min.css' );
-add_action( 'wp_enqueue_scripts', 'child_scripts' );
+function child_search_form(  ) {
+	$form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
+	<div class="input-group">	
+	<input type="text" class="form-control" value="' . get_search_query() . '" name="s" id="s" />
+	<span class="input-group-btn">
+	<button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+	</span>	
+	</div>
+	</form>';
+
+	return $form;
+}
+
+add_filter( 'get_search_form', 'child_search_form' );
+
+
+register_nav_menus( array(
+	'footer_menu' => 'Footer Menu',
+) );
+
